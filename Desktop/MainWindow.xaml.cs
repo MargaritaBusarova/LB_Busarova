73ce280b4550feda869b59dd90148da37b7c6519
Margarita Busarova <margarita.busarova13@gmail.com>
2024-10-13 20:55:59 +0300

    Добавьте файлы проекта.

---
+++ Desktop/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Desktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 win2 = new Window1();
+            win2.Show();
+            this.Close();
+        }
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            UpdateWatermark();
+        }
+
+        private void UpdateWatermark()
+        {
+            watermark.Visibility = string.IsNullOrWhiteSpace(textBox.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+        private void TextBox_TextChanged1(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            UpdateWatermark1();
+        }
+
+        private void UpdateWatermark1()
+        {
+            watermark1.Visibility = string.IsNullOrWhiteSpace(textBox1.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
---
+++ Desktop/Window1.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Desktop
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow win1 = new MainWindow();
+            win1.Show();
+            this.Close();
+        }
+        private void TextBox_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            UpdateWatermark();
+        }
+
+        private void UpdateWatermark()
+        {
+            watermark.Visibility = string.IsNullOrWhiteSpace(textBox.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+        private void TextBox_TextChanged1(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            UpdateWatermark1();
+        }
+
+        private void UpdateWatermark1()
+        {
+            watermark1.Visibility = string.IsNullOrWhiteSpace(textBox1.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+        private void TextBox_TextChanged2(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            UpdateWatermark2();
+        }
+
+        private void UpdateWatermark2()
+        {
+            watermark2.Visibility = string.IsNullOrWhiteSpace(textBox2.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        private void TextBox_TextChanged3(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            UpdateWatermark3();
+        }
+
+        private void UpdateWatermark3()
+        {
+            watermark3.Visibility = string.IsNullOrWhiteSpace(textBox3.Text) ? Visibility.Visible : Visibility.Collapsed;
+        }
+    }
+}
